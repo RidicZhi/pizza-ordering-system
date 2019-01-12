510cdc1a277c4a28cb6af447b03922af227885a2
HaoTong Zhi <s3637727@student.rmit.edu.au>
2019-01-12 17:19:58 +1100

    inin globalStyle and create landing page folder

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
+import { GlobalStyle } from './style';
+import Landing from './pages/landing';
 
 class App extends Component {
   render() {
     return (
-      <div >
-        pizza-ordering-app
-      </div>
+      <Fragment >
+        <GlobalStyle />
+        <Landing />
+      </Fragment>
     );
   }
 }
---
+++ src/pages/landing/index.js
@@ -0,0 +1,11 @@
+import React, { PureComponent } from 'react';
+
+class Landing extends PureComponent{
+  render(){
+    return(
+      <div>landing page</div>
+    )
+  }
+}
+
+export default Landing;
